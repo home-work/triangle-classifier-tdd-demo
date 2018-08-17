c419f1ac7d6130221ac4e1ea29afb985996904a8
datdta95 <datdta95@gmail.com>
2018-08-17 09:12:17 +0700

    refactor test method

--- src/test/java/TriangleClassifierTest.java
+++ src/test/java/TriangleClassifierTest.java
@@ -12,8 +12,8 @@ public class TriangleClassifierTest {
         int edge_2 = 2;
         int edge_3 = 2;
 
-        boolean expect = true;
-        boolean actual = TriangleClassifier.triangleClassifier(edge_1, edge_2, edge_3);
+        String expect = "Equilateral Triangle";
+        String actual = TriangleClassifier.triangleClassifier(edge_1, edge_2, edge_3);
         assertEquals(expect,actual);
     }
 }
