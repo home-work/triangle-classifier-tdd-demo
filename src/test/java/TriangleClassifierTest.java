478e290f9883a6bafae271b71dc56f4999b2abe1
datdta95 <datdta95@gmail.com>
2018-08-17 09:55:33 +0700

    Test: Pass với không phải là tam giác (có cạnh -1)

--- src/test/java/TriangleClassifierTest.java
+++ src/test/java/TriangleClassifierTest.java
@@ -1,12 +1,10 @@
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class TriangleClassifierTest {
 
     @Test
-    @DisplayName("Test value with a=b=c")
     public void triangleClassifierWith3EdgesAreEqual() {
         int edge_1 = 2;
         int edge_2 = 2;
@@ -17,7 +15,6 @@ public class TriangleClassifierTest {
         assertEquals(expect,actual);
     }
     @Test
-    @DisplayName("Test value with a=b")
     public void triangleClassifierWith2EdgesAreEqual() {
         int edge_1 = 2;
         int edge_2 = 2;
@@ -28,7 +25,6 @@ public class TriangleClassifierTest {
         assertEquals(expect,actual);
     }
     @Test
-    @DisplayName("Test value with a # b # c")
     public void triangleClassifierWith3DifferentEdges_1() {
         int edge_1 = 3;
         int edge_2 = 4;
@@ -39,11 +35,20 @@ public class TriangleClassifierTest {
         assertEquals(expect,actual);
     }
     @Test
-    @DisplayName("Test value with a # b # c")
     public void triangleClassifierWith3DifferentEdges_2() {
         int edge_1 = 8;
         int edge_2 = 2;
         int edge_3 = 3;
+
+        String expect = "Not Triangle";
+        String actual = TriangleClassifier.triangleClassifier(edge_1, edge_2, edge_3);
+        assertEquals(expect,actual);
+    }
+    @Test
+    public void triangleClassifierWith3DifferentEdges_3() {
+        int edge_1 = -1;
+        int edge_2 = 2;
+        int edge_3 = 1;
 
         String expect = "Not Triangle";
         String actual = TriangleClassifier.triangleClassifier(edge_1, edge_2, edge_3);
